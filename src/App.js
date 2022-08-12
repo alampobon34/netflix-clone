9fc76fbc551da9d387a17dbee19f4d271a384e18
Sayed Monshurul Alam <75158575+alampobon34@users.noreply.github.com>
2022-08-12 17:49:15 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
-
+import Row from "./components/Row";
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Row title="Netflix Original" />
+      <Row title="Trending Now" />
+    </div>
+  );
 }
 
 export default App;
